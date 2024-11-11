1cfaab6204ba8f50491cd77900212cd2e2ad9fe9
Abdellah Azzouz <abdellah.azzouz@student.vinci.be>
2024-11-11 16:53:00 +0100

    Refactoring the constant result

--- Spec/berlinClock.spec.js
+++ Spec/berlinClock.spec.js
@@ -2,17 +2,19 @@ import { Main } from "../Src/App.js";
 
 describe("The function should return the color of the lamp ", function() {
 
+    const result = new Main();
+
 
     it("Initially, all lamps are off  ,it should return (XXXX)", function() {
 
-        const result = new Main();
+       
 
         expect(result.getSimpleMinutes(0));
     });
 
     it("should return (YXXX) when the input is 1", function() {
 
-        const result = new Main();
+        
         
         expect(result.getSimpleMinutes(1));
     });
@@ -20,20 +22,21 @@ describe("The function should return the color of the lamp ", function() {
     
     it("should return (YYXX) when the input is 2", function() {
 
-        const result = new Main();
+        
         
         expect(result.getSimpleMinutes(2));
     });
 
     it("should return (YYYX) when the input is 3", function() {
 
-        const result = new Main();
+        
         
         expect(result.getSimpleMinutes(3));
     });
+    
     it("should return (YYYY) when the input is 4", function() {
 
-        const result = new Main();
+       
         
         expect(result.getSimpleMinutes(4));
     });
